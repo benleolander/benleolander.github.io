00f234ef9f825d1f3928a4ca8398ec9bb7c575f8
Ben Lander <ben@benlander.me>
2020-06-24 21:21:44 +0000

    eslint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function toggleTheme() {
-  !document.documentElement.hasAttribute("theme") ? document.documentElement.setAttribute("theme", "alternate") : document.documentElement.removeAttribute("theme")
+  !document.documentElement.hasAttribute('theme') ? document.documentElement.setAttribute('theme', 'alternate') : document.documentElement.removeAttribute('theme')
 }
 
-const themeButton = document.querySelector(".themeToggle")
+const themeButton = document.querySelector('.themeToggle')
 
 themeButton.addEventListener('click', toggleTheme)
